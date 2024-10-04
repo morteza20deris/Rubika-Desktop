80d9ae9acdeca0feb0f55b48cce21d7bfe254de2
Morteza Deris <mtz20dri@gmail.com>
2024-10-04 03:59:18 +0330

    add AutoUpdate

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow } = require("electron");
-
+const { autoUpdater } = require("electron-updater");
 function createWindow() {
 	const win = new BrowserWindow({
 		width: 800,
@@ -11,10 +11,20 @@ function createWindow() {
 	});
 
 	win.loadURL("https://web.rubika.ir"); // URL of the Rubika web app
+	autoUpdater.checkForUpdatesAndNotify();
 	win.setMenu(null);
 }
 
 app.whenReady().then(createWindow);
+
+autoUpdater.on("update-available", () => {
+	console.log("Update available.");
+});
+
+autoUpdater.on("update-downloaded", () => {
+	console.log("Update downloaded; will install now.");
+	autoUpdater.quitAndInstall(); // Automatically installs the update
+});
 
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") {

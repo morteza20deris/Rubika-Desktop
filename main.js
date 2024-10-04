eb651ce067350087a87492af4aedc7675acabe99
Morteza Deris <mtz20dri@gmail.com>
2024-10-04 04:32:03 +0330

    adding dialog to auto update

--- main.js
+++ main.js
@@ -17,13 +17,42 @@ function createWindow() {
 
 app.whenReady().then(createWindow);
 
-autoUpdater.on("update-available", () => {
-	console.log("Update available.");
+autoUpdater.on("update-available", (info) => {
+	dialog
+		.showMessageBox({
+			type: "info",
+			buttons: ["Update now", "Later"],
+			title: "Update Available",
+			message: `Version ${info.version} is available. Do you want to update now?`,
+		})
+		.then((result) => {
+			if (result.response === 0) {
+				// 'Update now' button clicked
+				// Download the update in the background
+				autoUpdater.downloadUpdate();
+			}
+		});
 });
 
 autoUpdater.on("update-downloaded", () => {
-	console.log("Update downloaded; will install now.");
-	autoUpdater.quitAndInstall(); // Automatically installs the update
+	dialog
+		.showMessageBox({
+			type: "info",
+			buttons: ["Install now", "Later"],
+			title: "Update Ready",
+			message:
+				"An update has been downloaded. Would you like to install it now?",
+		})
+		.then((result) => {
+			if (result.response === 0) {
+				// 'Install now' button clicked
+				autoUpdater.quitAndInstall(); // Quit and install the update
+			}
+		});
+});
+
+autoUpdater.on("error", (err) => {
+	console.error("Error during update:", err);
 });
 
 app.on("window-all-closed", () => {
